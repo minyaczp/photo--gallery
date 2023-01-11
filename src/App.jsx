d12ee4b3b67e99f013d237ac567268d82573f02e
minyaczp <minyaczp@gmail.com>
2023-01-11 21:25:48 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
 }
 
 export default App;
